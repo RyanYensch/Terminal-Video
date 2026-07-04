17531c1db8c311e5a70ac707c1eb12706550f5e4
Ryan Yensch <ryan@yensch.com>
2026-07-05 01:27:15 +1000

    got frame from video

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ bool downloaderInstalled() {
     return res == 0;
 }
 
+void convertFrame(cv::Mat frame) {
+    //TODO: convert frame into unicode
+}
+
 int main(int argc, char* argv[]) {
     if (!downloaderInstalled()) {
         std::cerr << "Error: 'yt-dlp' is not installed on your system PATH.\n";
@@ -15,12 +19,12 @@ int main(int argc, char* argv[]) {
         std::cerr << " Linux: sudo apt install yt-dlp OR pip3 instal yt-dlp.\n";
         std::cerr << " macOS: brew install yt-dlp.\n";
         std::cerr << "Please install it to use this player.\n";
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " https://www.youtube.com/watch?v=VIDEO_ID\n";
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     std::string command = "yt-dlp -f \"bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]\" -o video.mp4 " + std::string(argv[1]);
@@ -32,8 +36,32 @@ int main(int argc, char* argv[]) {
         std::cout << "Video download as video.mp4\n";
     } else {
         std::cerr << "Error retrieving video\n";
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
+    cv::VideoCapture cap("video.mp4");
+
+    if (!cap.isOpened()) {
+        std::cerr << "Error: Could not open video file.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    cv::Mat frame;
+
+    while (true) {
+        // Get the frame of the video
+        cap >> frame;
+
+        // Check if at the end of the video
+        if (frame.empty()) {
+            std::cout << "End of video.\n";
+            break;
+        }
+
+        std::cout << frame;
+        break;
+    }
+
+    cap.release();
     return 0;
 }
